75cabb24f2e65eb55ee62dc3296ccbc3ac7000b6
FatGuyy <omkarjadhav2103@gmail.com>
2023-03-05 12:41:01 +0530

    initial main file structure.

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+This is the main file for this project.
+"""
+import os
+import pandas as pd
+from datetime import date
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    
+    
+    main()
+
